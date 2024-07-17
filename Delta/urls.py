2ad9eda3aeec130c430616b4e2721405b220aa51
Fresm <best.timewon@gmail.com>
2024-07-17 19:19:34 +0900

    add file upload

--- Delta/urls.py
+++ Delta/urls.py
@@ -1,13 +1,16 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import FileUploadAPIView, ArticleViewSet, PostAPI
+from django.conf.urls.static import static
+from django.conf import settings
+from django.urls import path
 
 # router = DefaultRouter()
 # router.register(r'articles', ArticleViewSet)
 
 
 urlpatterns = [
-    path("<int:id>/", PostAPI),
+    path("<int:id>/", PostAPI), 
     path('upload-file/', FileUploadAPIView.as_view(), name='upload-file'),
 ]
 
