4b7241513c601a76234303b5fcc23679bcacbca6
Fresm <best.timewon@gmail.com>
2024-07-17 18:58:21 +0900

    add file upload

--- Delta/urls.py
+++ Delta/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import photo_upload, photo_list, FileUploadAPIView, ArticleViewSet
+from .views import FileUploadAPIView, ArticleViewSet
 
 # router = DefaultRouter()
 # router.register(r'articles', ArticleViewSet)
