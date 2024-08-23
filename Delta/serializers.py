ab45e89c00a14d1f7a808976b96516c2513f2db8
Fresm <best.timewon@gmail.com>
2024-08-23 11:41:12 +0900

    add ost Feature

--- Delta/serializers.py
+++ Delta/serializers.py
@@ -4,7 +4,7 @@ from .models import UploadedFile, Delta
 class DeltaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Delta
-        fields = ['title', 'picture', 'detail', 'idea', 'studyTime', 'profile']
+        fields = ['title', 'picture', 'detail', 'idea', 'studyTime']
 
 class FileUploadSerializer(serializers.ModelSerializer):
     class Meta:
