57887edae54e64d749b1bd0fda540dfe63ef9c2b
Fresm <best.timewon@gmail.com>
2024-08-29 13:18:32 +0900

    add ost Feature

--- myapi/settings.py
+++ myapi/settings.py
@@ -86,9 +86,8 @@ DATABASES = {
     }
 }
 
-if "DATABASE_URL" in os.environ:
-    db_from_env = dj_database_url.config(conn_max_age=500)
-    DATABASES["default"].update(db_from_env)
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES["default"].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
